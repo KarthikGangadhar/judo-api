77f14054368569c8a86c29872a556778adefd4c1
karthik <karthikg1643@gmail.com>
2018-02-27 02:04:57 +0530

    added cricketers endpoints to athletes api

--- app/helpers/routes.js
+++ app/helpers/routes.js
@@ -2,6 +2,7 @@
 
 const Joi = require('joi');
 const Handlers = require('../lib/handlers.js');
+const crickerHandlers = require('../lib/cricker_handlers.js');
 
 const objectSchema = Joi.object({
     year: Joi.string(),
@@ -21,6 +22,70 @@ const athleteSchema = {
     link: Joi.string(),
     medals: Joi.array().items(objectSchema).unique()
 };
+
+const battingSchema = Joi.object({
+    "50": Joi.string(),
+    "100": Joi.string(),
+    "St": Joi.string(),
+    "Ct": Joi.string(),
+    "6s": Joi.string(),
+    "4s": Joi.string(),
+    "SR": Joi.string(),
+    "BF": Joi.string(),
+    "Ave": Joi.string(),
+    "HS": Joi.string(),
+    "Runs": Joi.string(),
+    "NO": Joi.string(),
+    "Inns": Joi.string(),
+    "Mat": Joi.string()
+});
+
+const bowlingSchema = Joi.object({
+    "10": Joi.string(),
+    "5w": Joi.string(),
+    "4w": Joi.string(),
+    "SR": Joi.string(),
+    "Econ": Joi.string(),
+    "Ave": Joi.string(),
+    "BBM": Joi.string(),
+    "BBI": Joi.string(),
+    "Wkts": Joi.string(),
+    "Runs": Joi.string(),
+    "Balls": Joi.string(),
+    "Inns": Joi.string(),
+    "Mat": Joi.string()
+})
+
+const playerSchema = {
+    "pid": Joi.string().required(),
+    "profile": Joi.string(),
+    "imageURL": Joi.string(),
+    "battingStyle": Joi.string(),
+    "bowlingStyle": Joi.string(),
+    "majorTeams": Joi.string(),
+    "currentAge": Joi.string(),
+    "born": Joi.string(),
+    "fullName": Joi.string(),
+    "name": Joi.string().required(),
+    "country": Joi.string().required(),
+    "playingRole": Joi.string(),
+    "data": Joi.object({
+        "bowling": Joi.object({
+            "listA": bowlingSchema,
+            "firstClass": bowlingSchema,
+            "T20Is": bowlingSchema,
+            "ODIs": bowlingSchema,
+            "tests": bowlingSchema
+        }),
+        "batting": Joi.object({
+            "listA": battingSchema,
+            "firstClass": battingSchema,
+            "T20Is": battingSchema,
+            "ODIs": battingSchema,
+            "tests": battingSchema
+        })
+    })
+}
 
 const resultHTTPStatus = {
     '200': {
@@ -72,7 +137,7 @@ module.exports = [{
     },
     {
         method: 'GET',
-        path: '/athletes/{athleteId}',
+        path: '/athletes/:athleteId',
         config: {
             handler: Handlers.getByIdUser,
             description: 'Get athletes By ID',
@@ -119,6 +184,102 @@ module.exports = [{
             description: 'Delete a athlete Data',
             tags: ['api', 'reduced'],
             notes: ['Update a athletes data by Id'],
+            plugins: {
+                'hapi-swagger': {
+                    responses: resultHTTPStatus
+                }
+            },
+            validate: {
+                payload: {
+                    _id: Joi.string().required(),
+                    id: Joi.string().required(),
+                    name: Joi.string().required(),
+                }
+            }
+        }
+    }, ///////////////////
+    {
+        method: 'GET',
+        path: '/crickers',
+        config: {
+            handler: crickerHandlers.getAllUsers,
+            description: 'Get All Crickers',
+            tags: ['api', 'reduced'],
+            notes: ['Fetches all the existing crickers data from mongodb'],
+            plugins: {
+                'hapi-swagger': {
+                    responses: resultHTTPStatus
+                }
+            }
+        }
+    },
+    {
+        method: 'POST',
+        path: '/crickers',
+        config: {
+            handler: crickerHandlers.createUser,
+            description: 'Create New crickers',
+            tags: ['api', 'reduced'],
+            notes: ['Create a new crickers and updates data'],
+            plugins: {
+                'hapi-swagger': {
+                    responses: resultHTTPStatus
+                }
+            },
+            validate: {
+                payload: playerSchema
+            }
+        }
+    },
+    {
+        method: 'GET',
+        path: '/crickers/{crickerId}',
+        config: {
+            handler: crickerHandlers.getByIdUser,
+            description: 'Get crickers By ID',
+            tags: ['api', 'reduced'],
+            notes: ['Fetches the existing crickers data by Id'],
+            plugins: {
+                'hapi-swagger': {
+                    responses: resultHTTPStatus
+                }
+            },
+            validate: {
+                query: {
+                    id: Joi.string().required().description('Id: cricker Id')
+                }
+            }
+        }
+    },
+    {
+        method: 'PUT',
+        path: '/crickers/{crickerId}',
+        config: {
+            handler: crickerHandlers.updateUser,
+            description: 'Update existing cricker Data',
+            tags: ['api', 'reduced'],
+            notes: ['Update a crickers data by Id'],
+            plugins: {
+                'hapi-swagger': {
+                    responses: resultHTTPStatus
+                }
+            },
+            validate: {
+                payload: playerSchema,
+                query: {
+                    id: Joi.string().required().description('Id: cricker Id')
+                }
+            }
+        }
+    },
+    {
+        method: 'DELETE',
+        path: '/crickers/{crickerId}',
+        config: {
+            handler: crickerHandlers.deleteUser,
+            description: 'Delete a cricker Data',
+            tags: ['api', 'reduced'],
+            notes: ['Update a crickers data by Id'],
             plugins: {
                 'hapi-swagger': {
                     responses: resultHTTPStatus
--- config.js
+++ config.js
@@ -1,6 +1,7 @@
 module.exports = {
     'secret': 'ramataranamaka',
     'database': 'mongodb://kgangadhar:takanami1643@ds249128.mlab.com:49128/jodo',
+    'playerstat':'mongodb://kgangadhar:takanami1643@ds249718.mlab.com:49718/playerstats',
     'token_message': "Expires in %s Minutes",
     'Authentication_messages': {
         token_required: {
